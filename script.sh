a55d0aaebb68f6d6fc8adbe8b0baffd0b6d16a19
Rishabh9818 <142471691+Rishabh9818@users.noreply.github.com>
2024-01-18 12:56:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,18 +10,23 @@ logInfoMessage "Scanning the code available at [$WORKSPACE] and mounted at [$COD
 code="$WORKSPACE/$CODEBASE_DIR"
 sleep $SLEEP_DURATION
 cd  $code
-logInfoMessage "Received arguments: [$@]"
 
+#logInfoMessage "Received arguments: [$@]"
 # Perform license scanning using scancode
+
 scancode -cl --license --html $filename .
 # scancode -cl --license --html /path/to/port_name.html .
+if [ $? -eq 0 ]
+then
 # Check the exit status of scancode
-if scancode -cl --license --html "$filename" .; then
+#if scancode -cl --license --html "$filename" .; then
+  generateOutput scancode_execute true "License scan succeeded!"
   logInfoMessage "License scan succeeded!"
-  generateOutput mvn_execute true "License scan succeeded!"
-else
+  echo "License scan succeeded!" 
+elif
+then
+  generateOutput scancode_execute false "License scan failed!"
   logErrorMessage "License scan failed!"
-  generateOutput mvn_execute false "License scan failed!"
   echo "Build unsuccessful"
   exit 1
 fi
