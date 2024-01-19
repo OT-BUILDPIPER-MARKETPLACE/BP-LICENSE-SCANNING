2322e81b6a6cc71ff7706f9eb39e00ba03045ef2
Rishabh9818 <142471691+Rishabh9818@users.noreply.github.com>
2024-01-19 12:36:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-#source functions.sh
-#source log-functions.sh
-#source str-functions.sh
-#source file-functions.sh
-#source aws-functions.sh
+source functions.sh
+source log-functions.sh
+source str-functions.sh
+source file-functions.sh
+source aws-functions.sh
 
-#logInfoMessage "Scanning the code available at [$WORKSPACE] and mounted at [$CODEBASE_DIR]"
+logInfoMessage "Scanning the code available at [$WORKSPACE] and mounted at [$CODEBASE_DIR]"
 code="$WORKSPACE/$CODEBASE_DIR"
 sleep $SLEEP_DURATION
 cd  $code
-
-#logInfoMessage "Received arguments: [$@]"
-# Perform license scanning using scancode
-
+logInfoMessage "Received arguments: [$@]"
 scancode -cl --license --html $OUTPUT_ARG .
-# scancode -cl --license --html /path/to/port_name.html .
+
 if [ $? -eq 0 ];
 then
-# Check the exit status of scancode
 #if scancode -cl --license --html "$filename" .; then
-#  generateOutput scancode_execute false "License scan succeeded!"
-#  logInfoMessage "License scan succeeded!"
+  generateOutput scancode_execute false "License scan succeeded!"
+  logInfoMessage "License scan succeeded!"
   echo "License scan succeeded!" 
 else
-#  generateOutput scancode_execute false "License scan failed!"
-#  logErrorMessage "License scan failed!"
-#  echo "Build unsuccessful"
+  generateOutput scancode_execute false "License scan failed!"
+  logErrorMessage "License scan failed!"
+  echo "Build unsuccessful"
   exit 1
 fi
